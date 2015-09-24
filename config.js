fcb6519ca66716bddcc5824c78d907c784582603
umiringo42@gmail.com <umiringo42@gmail.com>
2015-09-24 16:07:00 +0800

    add readme

---
+++ config.js
@@ -0,0 +1,24 @@
+//net
+var net = {
+	port : 8878,
+	salt : 'Sz6MwPzxSNjzHcRbC85y2a1OtJHx7GAJ',
+	version : 1.0.0
+};
+
+//mongodb
+var mongodb = {
+	path : 'mongodb://localhost/glyptodon'
+};
+
+//log
+var log = {
+	debug : './log/debug.log',
+	verbose : './log/verbose.log',
+	info : './log/info.log',
+	error : './log/error.log',
+	warn : './log/warn.log'
+};
+
+exports.net = net;
+exports.mongodb = mongodb;
+exports.log = log;
